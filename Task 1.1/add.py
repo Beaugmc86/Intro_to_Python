3a9bbd8f70b48964542f094adfeae15c5547dd12
Beaugmc86 <153542400+Beaugmc86@users.noreply.github.com>
2024-09-05 15:45:24 -0700

    initial commit

---
+++ Task 1.1/add.py
@@ -0,0 +1,7 @@
+a = int(input("Enter number for a: "))
+
+b = int(input(" Enter number for b: "))
+
+c = a + b
+
+print("sum of", a, "+", b, "=", c)
---
+++ TestScripts/add.py
@@ -0,0 +1,7 @@
+a = int(input("Enter the first number: "))
+
+b = int(input(" Enter the second number: "))
+
+c = a + b
+
+print("sum of", a, "and", b, "is:", c)
