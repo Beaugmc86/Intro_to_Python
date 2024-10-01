1edf7adc832231a9ecd6a318575d88ce11cf5cbb
Beaugmc86 <153542400+Beaugmc86@users.noreply.github.com>
2024-10-01 14:54:40 -0700

    Update for line spacing, functional changes, design elements, and screenshots of coding.
    create_recipe() - allow for user input to be capitalized if input as lowercase.
    
    search_by_ingredient() - updated so searched ingredient will output all recipes where that ingredient is used.
    
    edit_recipe() - updated so new names could contain a space if recipe is more than one word.

--- Task_1.7/recipe_app.py
+++ Task_1.7/recipe_app.py
@@ -4,6 +4,7 @@ from sqlalchemy import Column
 from sqlalchemy.types import Integer, String
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import re
 
 # Connecting SQLAlchemy with the database
 engine = create_engine("mysql://cf-python:password@localhost/task_database")
@@ -14,6 +15,7 @@ Base = declarative_base()
 # Create session object to make changes to database
 Session = sessionmaker(bind=engine)
 session = Session()
+
 
 # Define Recipe model
 class Recipe(Base):
@@ -70,9 +72,9 @@ def create_recipe():
     while True:
         name = input("Enter name of recipe: ")
         if len(name) > 50:
-            print("Error: Recipe name cannot be longer than 50 characters.")
-        elif not name.isalpha():
-            print("Error: Recipe name can only contain letters.")
+            print("\nError: Recipe name cannot be longer than 50 characters.")
+        elif not name.replace(" ", "").isalpha():
+            print("\nError: Recipe name can only contain letters.")
         else:
             break
     
@@ -80,7 +82,7 @@ def create_recipe():
     while True:
         cooking_time = input("Enter recipe cooking time in minutes: ")
         if not cooking_time.isnumeric():
-            print("Error: Cooking time must be a number.")
+            print("\nError: Cooking time must be a number.")
         else:
             cooking_time = int(cooking_time)
             break
@@ -90,7 +92,7 @@ def create_recipe():
     n = int(input("How many ingredients would you like to enter?: "))
 
     for i in range(n):
-        ingredient = input(f'Enter ingredient {i+1}: ')
+        ingredient = input(f'Enter ingredient {i+1}: ').strip().title()
         ingredients.append(ingredient)
 
     # Convert ingredients list to string
@@ -110,7 +112,7 @@ def create_recipe():
     session.add(recipe_entry)
     session.commit()
 
-    print("Recipe successfully added!")
+    print("\nRecipe successfully added!")
 
 # View all recipes
 def view_all_recipes():
@@ -119,7 +121,7 @@ def view_all_recipes():
 
     # Check if list is empty
     if not recipes_list:
-        print("No recipes found in the database.")
+        print("\nNo recipes found in the database.")
         return None
     
     # Call __str__ method to print all recipes
@@ -131,7 +133,7 @@ def search_by_ingredients():
     # Query to check for table entries
     recipe_count = session.query(Recipe).count()
     if recipe_count == 0:
-        print("No recipes found in the database.")
+        print("\nNo recipes found in the database.")
         return None
     
     # Query to retrieve ingredients column only
@@ -151,17 +153,17 @@ def search_by_ingredients():
     print("\nIngredients available: ")
     for idx, ingredient in enumerate(all_ingredients, start=1):
         print(f"{idx}. {ingredient}")
-    selected_ingredients = input("Enter the numbers of ingredients you'd like to search, separated by spaces: ")
+    selected_ingredients = input("\nEnter the numbers of ingredients you'd like to search, separated by spaces: ")
 
     # Validate user's input
     try:
         selected_indices = [int(i) for i in selected_ingredients.split()]
     except ValueError:
-        print("Input invalid. Please enter numbers separated by spaces.")
+        print("\nInput invalid. Please enter numbers separated by spaces.")
         return None
     
     if any(i < 1 or i > len(all_ingredients) for i in selected_indices):
-        print("Error: Selection invalid. Please enter numbers from the displayed options.")
+        print("\nError: Selection invalid. Please enter numbers from the displayed options.")
         return None
     
     # Make list of selected ingredients to search for
@@ -176,12 +178,12 @@ def search_by_ingredients():
         conditions.append(Recipe.ingredients.like(like_term))
 
     # Retrieve recipes from the database using filter() with conditions
-    # Use 'and_' to combine the conditions so that all ingredients must be present
-    from sqlalchemy import and_
-    matching_recipes = session.query(Recipe).filter(and_(*conditions)).all()
+    # Use 'or_' to combine the conditions so that all ingredients must be present
+    from sqlalchemy import or_
+    matching_recipes = session.query(Recipe).filter(or_(*conditions)).all()
 
     if not matching_recipes:
-        print("No recipes found including all your selected ingredients.")
+        print("\nNo recipes found including all your selected ingredients.")
     else:
         for recipe in matching_recipes:
             print(recipe)
@@ -191,7 +193,7 @@ def edit_recipe():
     # Check if recipe exists
     recipe_count = session.query(Recipe).count()
     if recipe_count == 0:
-        print("Recipe does not exist in database.")
+        print("\nRecipe does not exist in database.")
         return None
     
     # Retrieve id and name of each recipe
@@ -203,37 +205,38 @@ def edit_recipe():
         print(f"ID: {recipe.id} - Name: {recipe.name}")
 
     # Prompt user to select recipe by ID to update
-    selected_id = input("Enter the ID of the recipe you would like to update: ")
+    selected_id = input("\nEnter the ID of the recipe you would like to update: ")
     if not selected_id.isnumeric() or int(selected_id) not in [r.id for r in results]:
-        print("Error: Recipe must be selected by its ID. Please try again.")
+        print("\nError: Recipe must be selected by its ID. Please try again.")
         return None
     
     # Retrieve recipe of selected ID
     recipe_to_edit = session.query(Recipe).filter_by(id=int(selected_id)).one()
 
     # Print the selected recipe details
-    print("Recipe details:")
+    print("\nRecipe details:")
     print(f"1. Name: {recipe_to_edit.name}")
     print(f"2. Ingredients: {recipe_to_edit.ingredients}")
     print(f"3. Cooking Time: {recipe_to_edit.cooking_time} minutes")
 
     # Prompt user for field to update
-    choice = input("enter the number of the field 1, 2, or 3 to update: ")
+    choice = input("\nEnter the number of the field 1, 2, or 3 to update: ")
     if choice not in ["1", "2", "3"]:
-        print("Invalid choice. Please choose 1, 2, or 3.")
+        print("\nInvalid choice. Please choose 1, 2, or 3.")
         return None
     
     # Update attribute based on user's choice
     if choice == "1":
-        new_name = input("Enter new recipe name: ")
-        if len(new_name) > 50 or not new_name.isalpha():
-            print("Error: Recipe's new name must be letters only and no longer than 50 characters.")
+        new_name = input("\nEnter new recipe name: ")
+        # re.match("^[A-Za-z ]+$", new_name) allows for spaces in updated recipe names
+        if len(new_name) > 50 or not re.match("^[A-Za-z ]+$", new_name):
+            print("\nError: Recipe's new name must be letters only and no longer than 50 characters.")
             return None
         recipe_to_edit.name = new_name
 
     elif choice == "2":
         new_ingredients = []
-        n = int(input("How many ingredients would you like to add?: "))
+        n = int(input("\nHow many ingredients are in this recipe?: "))
 
         for i in range(n):
             ingredient = input(f"Enter ingredient {i+1}: ")
@@ -242,9 +245,9 @@ def edit_recipe():
         recipe_to_edit.ingredients = ", ".join(new_ingredients)
 
     if choice == "3":
-        new_time = input("Enter the new cooking time in minutes: ")
+        new_time = input("\nEnter the new cooking time in minutes: ")
         if not new_time.isnumeric():
-            print("Error: Cooking time must be entered as a number.")
+            print("\nError: Cooking time must be entered as a number.")
             return None
         recipe_to_edit.cooking_time = int(new_time)
     
@@ -253,13 +256,13 @@ def edit_recipe():
 
     session.commit()
 
-    print("Recipe has been updated successfully!")
+    print("\nRecipe has been updated successfully!")
 
 def delete_recipe():
     # Check if recipe exists
     recipe_count = session.query(Recipe).count()
     if recipe_count == 0:
-        print("Recipe does not exist in database.")
+        print("\nRecipe does not exist in database.")
         return None
     
     # Retrieve id and name of each recipe
@@ -268,12 +271,12 @@ def delete_recipe():
     # Display recipes to user
     print("\nAvailable recipes:")
     for recipe in results:
-        print(f"ID: {recipe.id} - Name: {recipe.name}")
+        print(f"{recipe.id}. {recipe.name}")
 
     # Prompt user to select recipe by ID to update
     selected_id = input("Enter the ID of the recipe you would like to delete: ")
     if not selected_id.isnumeric() or int(selected_id) not in [r.id for r in results]:
-        print("Error: Recipe must be selected by its ID. Please try again.")
+        print("\nError: Recipe must be selected by its ID. Please try again.")
         return None
     
     # Convert input to integer
@@ -283,47 +286,56 @@ def delete_recipe():
     recipe_to_delete = session.query(Recipe).filter_by(id=selected_id).one()
 
     # Confirm deletion
-    choice = input(f"Are you sure you want to delete recipe '{recipe_to_delete.name}'? Enter 'Y' or 'N': ").lower()
+    choice = input(f"Are you sure you want to delete recipe '{recipe_to_delete.name}'? Enter 'Y' or 'N': ")
 
     # Action based on user choice
-    if choice == 'N':
-        print("Recipe not deleted.")
+    if choice == 'N' or choice == 'n':
+        print("\nRecipe not deleted.")
         return None
     
-    elif choice == 'Y':
+    elif choice == 'Y' or choice == 'y':
         session.delete(recipe_to_delete)
         session.commit()
-        print("Recipe has been deleted.")
+        print("\nRecipe has been deleted.")
 
     else:
-        print("Invalid choice. You must enter 'Y' or 'N'.")
+        print("\nInvalid choice. You must enter 'Y' or 'N'.")
         return None
 
 # Define main_menu function
 def main_menu():
     while True:
+        print("\n-----------------------")
+        print("Welcome to Recipe App!")
+        print("-----------------------")
         print("\nMain Menu:")
         print("1. Create a new recipe")
-        print("2. Search for a recipe by ingredient")
-        print("3. Update an existing recipe")
-        print("4. Delete a recipe")
-        print("5. Exit")
+        print("2. View all recipes")
+        print("3. Search for a recipe by ingredient")
+        print("4. Update an existing recipe")
+        print("5. Delete a recipe")
+        print("6. Exit")
 
-        choice = input("Enter your choice: ")
+        choice = input("\nEnter your choice: ")
 
         if choice == '1':
             create_recipe()
         elif choice == '2':
-            search_by_ingredients()
+            view_all_recipes()
         elif choice == '3':
-            edit_recipe()
+            search_by_ingredients()
         elif choice == '4':
-            delete_recipe()
+            edit_recipe()
         elif choice == '5':
-            print("Exiting program...")
+            delete_recipe()
+        elif choice == '6':
+            print("Exiting program.")
+            print("\n-------------------------------")
+            print("Thank you for using Recipe App!")
+            print("-------------------------------\n")
             break
         else:
-            print("Invalid choice. Please try again.")
+            print("\nInvalid choice. Please try again.")
 
     # Close the session before exiting
     session.close()
